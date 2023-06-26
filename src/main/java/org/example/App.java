57c9b2088f5f6df48d4954e9c8cff8c6cc589155
StasyaJeki <stasya-a13@mail.ru>
2023-06-26 17:10:59 +0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.example.config.MyConfig;
+import org.example.entity.Communication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args ){
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication = context.getBean("communication", Communication.class);
+        System.out.println("Answer: " + communication.getAnswer());
+    }
+}
